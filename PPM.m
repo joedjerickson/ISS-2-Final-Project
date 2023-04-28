ae42662356dda8c550867b4eb6709e264d05bab7
Aidan Ferry <machjayus@gmail.com>
2023-04-28 13:25:02 -0600

    Update PPM.m

--- PPM.m
+++ PPM.m
@@ -1,29 +1,33 @@
-dataSequenceTrain = 'sequence_solarWind_train';
-dataSequenceTest = 'sequence_solarWind_test';
+% Mixed PPM Scheme Thing
+% by Aidan Ferry
 
-%nth order mixed PPM thing
-test = false;
-order = 3;
+dataSequenceTrain = 'sequence_Dickens_train';
+dataSequenceTest = 'sequence_Dickens_test';
+
+test = true;
+order = 5;
 if(test==false)
     sequenceLength = initializeSymbolMachine(dataSequenceTrain);
 else
     sequenceLength = initializeSymbolMachine(dataSequenceTest);
 end
-%symbolCounts = ones((10/9)*((10^order)-1),9); %Default distribution. Uncomment for the
+symbolCounts = ones((10/9)*((10^order)-1),9); %Default distribution. Uncomment for the
 %first time that you try new data or a new order chain
-symbolCounts = readmatrix("Z:\Desktop\Classes\S23\ISS II\"+dataSequenceTrain+".csv"); %Prior known distribution from training
+%symbolCounts = readmatrix("Z:\Desktop\Classes\S23\ISS II\"+dataSequenceTrain+".csv"); %Prior known distribution from training
 last = 0;
+t=1:1:order;
+weights = t;
 for ii = 1:sequenceLength
     probs = zeros(1,9);
     for j = 1:order
-        probs = probs + symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,:)/sum(symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,:));
+        probs = probs + weights(j)*symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,:)/sum(symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,:));
     end
-    probs = probs./order; %Straight up average
+    probs = probs./sum(probs);
     [thisSymbol,penalty] = symbolMachine(probs);
     for j = 1:order
         symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,thisSymbol) = symbolCounts((10/9)*(10^(j-1)-1)+mod(last,10^j)+1,thisSymbol) + 1;
     end
-
+    
     last = mod(last,10^(order-1));
     last = last * 10;
     last = last + thisSymbol;
